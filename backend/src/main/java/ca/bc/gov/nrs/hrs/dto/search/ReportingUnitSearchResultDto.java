f1bc2699cbb5f6fe98f20f441d23098121921038
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-08-27 16:59:32 -0700

    feat(#67): add DTOs for reporting unit search parameters and results

---
+++ backend/src/main/java/ca/bc/gov/nrs/hrs/dto/search/ReportingUnitSearchResultDto.java
@@ -0,0 +1,18 @@
+package ca.bc.gov.nrs.hrs.dto.search;
+
+import ca.bc.gov.nrs.hrs.dto.base.CodeDescriptionDto;
+import java.time.LocalDateTime;
+import lombok.With;
+
+@With
+public record ReportingUnitSearchResultDto(
+    String blockId,
+    Long ruNumber,
+    CodeDescriptionDto client,
+    CodeDescriptionDto clientLocation,
+    CodeDescriptionDto sampling,
+    CodeDescriptionDto district,
+    CodeDescriptionDto status,
+    LocalDateTime lastUpdated
+) {
+}
