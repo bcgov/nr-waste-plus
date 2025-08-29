354acdc5477af137150d0117be3672de4e9596e4
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-08-28 17:06:21 -0700

    fix: update removeEmpty function to filter out null values

--- frontend/src/services/utils.ts
+++ frontend/src/services/utils.ts
@@ -1,4 +1,4 @@
 export const removeEmpty = <T extends object>(obj: T): Partial<T> => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != null)) as Partial<T>;
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v)) as Partial<T>;
 };
