4984325f13db32b84a931239d1c0bff4a787ba30
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-09-03 05:40:03 -0700

    fix(#83): fixing env vars

--- frontend/src/services/APIs.ts
+++ frontend/src/services/APIs.ts
@@ -1,4 +1,5 @@
 import { getUserTokenFromCookie } from '@/context/auth/authUtils';
+import { env } from '@/env';
 import { SearchService } from '@/services//search.service';
 import { ForestClientService } from '@/services/forestclient.service';
 import { UserService } from '@/services/users.service';
@@ -8,7 +9,7 @@ import { CodesService } from './codes.service';
 import type { APIConfig } from '@/config/api/types';
 
 export const BackendApiConfig: APIConfig = {
-  BASE: 'http://localhost:8080',
+  BASE: env.VITE_BACKEND_URL,
   VERSION: '0',
   WITH_CREDENTIALS: true,
   CREDENTIALS: 'include',
