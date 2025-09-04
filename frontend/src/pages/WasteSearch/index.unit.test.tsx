36ab4ab69154a445d02e4330f9279682abe7955b
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-09-04 10:10:01 -0700

    test(#83): fixing test for WasteSearchPage

--- frontend/src/pages/WasteSearch/index.unit.test.tsx
+++ frontend/src/pages/WasteSearch/index.unit.test.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { act, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { describe, it, expect, type Mock, beforeEach } from 'vitest';
+import { describe, it, expect, type Mock, beforeEach, vi } from 'vitest';
 
 import PageTitleProvider from '@/context/pageTitle/PageTitleProvider';
 import { PreferenceProvider } from '@/context/preference/PreferenceProvider';
