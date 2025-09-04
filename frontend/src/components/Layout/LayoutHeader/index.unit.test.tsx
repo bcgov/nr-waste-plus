95f4760385c9414dae99ec83563c78be9d0eaab3
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-09-04 08:16:55 -0700

    test(#83): renaming test for LayoutHeader

--- frontend/src/components/Layout/LayoutHeader/index.unit.test.tsx
+++ frontend/src/components/Layout/LayoutHeader/index.unit.test.tsx
@@ -1,13 +1,25 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen, fireEvent, act } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi, beforeEach, type Mock } from 'vitest';
 
 import { LayoutHeader } from '@/components/Layout/LayoutHeader';
 import { AuthProvider } from '@/context/auth/AuthProvider';
 import { LayoutProvider } from '@/context/layout/LayoutProvider';
 import { PreferenceProvider } from '@/context/preference/PreferenceProvider';
 import ThemeProvider from '@/context/theme/ThemeProvider';
+import APIs from '@/services/APIs';
+
+vi.mock('@/services/APIs', () => {
+  return {
+    default: {
+      user: {
+        getUserPreferences: vi.fn(),
+        updateUserPreferences: vi.fn(),
+      },
+    },
+  };
+});
 
 const renderWithProviders = async () => {
   const qc = new QueryClient();
@@ -31,6 +43,11 @@ const renderWithProviders = async () => {
 };
 
 describe('LayoutHeader', () => {
+  beforeEach(() => {
+    (APIs.user.getUserPreferences as Mock).mockResolvedValue({ theme: 'g10' });
+    (APIs.user.updateUserPreferences as Mock).mockResolvedValue({});
+  });
+
   it('renders header with title Waste Plus', async () => {
     await renderWithProviders();
     const header = await screen.findByTestId('bc-header__header');
