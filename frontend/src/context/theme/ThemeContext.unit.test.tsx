cb79558da1a75de27562de2dcffb7d66f2eb8bbe
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-09-04 08:07:21 -0700

    test(#83): fixed test

--- frontend/src/context/theme/ThemeContext.unit.test.tsx
+++ frontend/src/context/theme/ThemeContext.unit.test.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { waitFor } from '@testing-library/dom';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, it, expect, vi, type Mock } from 'vitest';
 
 import { PreferenceProvider } from '@/context/preference/PreferenceProvider';
@@ -8,7 +10,6 @@ import APIs from '@/services/APIs';
 
 import { ThemeProvider } from './ThemeProvider';
 import { useTheme } from './useTheme';
-import userEvent from '@testing-library/user-event';
 
 vi.mock('@/services/APIs', () => {
   return {
