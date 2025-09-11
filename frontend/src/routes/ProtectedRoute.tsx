3a1bc909ddb0f3762b9b6efdd3b5d4af51866cde
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-09-11 16:26:30 -0700

    fix: fixing tests

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -2,17 +2,22 @@ import { Navigate } from 'react-router-dom';
 
 import { useAuth } from '@/context/auth/useAuth';
 
+import type { FamRole } from '@/context/auth/types';
+
 export default function ProtectedRoute({
   children,
   roles,
 }: {
   children: React.ReactNode;
-  roles?: string[];
+  roles?: FamRole[];
 }) {
   const { user } = useAuth();
 
   if (!user) return <Navigate to="/login" />;
-  if (roles && !roles.some((role) => user.roles?.includes(role))) {
+  if (
+    roles &&
+    !roles.some((role) => user.roles?.map((userRole) => userRole.role).includes(role.role))
+  ) {
     return <Navigate to="/unauthorized" />;
   }
 
