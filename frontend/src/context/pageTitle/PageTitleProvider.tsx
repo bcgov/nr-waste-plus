e141779ec38108764ea8d78c2ae3ab155b405c49
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-10-29 09:39:58 -0700

    fix: fixing sonar warnings

--- frontend/src/context/pageTitle/PageTitleProvider.tsx
+++ frontend/src/context/pageTitle/PageTitleProvider.tsx
@@ -1,28 +1,25 @@
-import { useState, useEffect, type ReactNode, useCallback } from 'react';
+import { useState, useEffect, type ReactNode, useCallback, useMemo } from 'react';
 
 import { env } from '@/env';
 
 import { PageTitleContext } from './PageTitleContext';
 
 export const PageTitleProvider = ({ children }: { children: ReactNode }) => {
-  const [pageTitle, setPageTitleState] = useState(env.VITE_APP_NAME);
+  const [pageTitle, setPageTitle] = useState(env.VITE_APP_NAME);
   const [currentHierarchy, setCurrentHierarchy] = useState(0);
 
   const resolveName = (title: string) =>
     [env.VITE_APP_NAME, title].join(' - ') ?? env.VITE_APP_NAME;
 
-  const setPageTitle = useCallback(
-    (title: string, hierarchy?: 1 | 2 | 3) => {
-      const actualHierarchy = hierarchy ?? 3;
-      setPageTitleState((prev) => {
-        if (actualHierarchy >= currentHierarchy) {
+  const updatePageTitle = useCallback(
+    (title: string, hierarchy: 1 | 2 | 3 = 3) => {
+      setPageTitle((prev) => {
+        if (hierarchy >= currentHierarchy) {
           return resolveName(title);
         }
         return prev;
       });
-      setCurrentHierarchy((prevHierarchy) =>
-        actualHierarchy >= prevHierarchy ? actualHierarchy : prevHierarchy,
-      );
+      setCurrentHierarchy((prevHierarchy) => Math.max(hierarchy, prevHierarchy));
     },
     [currentHierarchy],
   );
@@ -31,11 +28,16 @@ export const PageTitleProvider = ({ children }: { children: ReactNode }) => {
     document.title = pageTitle;
   }, [pageTitle]);
 
-  return (
-    <PageTitleContext.Provider value={{ pageTitle, setPageTitle, currentHierarchy }}>
-      {children}
-    </PageTitleContext.Provider>
+  const providerValue = useMemo(
+    () => ({
+      pageTitle,
+      setPageTitle: updatePageTitle,
+      currentHierarchy,
+    }),
+    [pageTitle, updatePageTitle, currentHierarchy],
   );
+
+  return <PageTitleContext.Provider value={providerValue}>{children}</PageTitleContext.Provider>;
 };
 
 export default PageTitleProvider;
