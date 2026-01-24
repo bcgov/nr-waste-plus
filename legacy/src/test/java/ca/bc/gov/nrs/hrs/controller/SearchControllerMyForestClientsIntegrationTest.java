b3a45130c360fdf05032224dc288fcdc0c4e1bf8
Maria Martinez <maria.martinez@gov.bc.ca>
2026-01-23 20:57:12 -0800

    no message

--- legacy/src/test/java/ca/bc/gov/nrs/hrs/controller/SearchControllerMyForestClientsIntegrationTest.java
+++ legacy/src/test/java/ca/bc/gov/nrs/hrs/controller/SearchControllerMyForestClientsIntegrationTest.java
@@ -57,7 +57,7 @@ class SearchControllerMyForestClientsIntegrationTest extends
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.content.length()").value(0))
         .andExpect(jsonPath("$.page.size").value(10))
-        .andExpect(jsonPath("$.page.totalElements").value(2))
+        .andExpect(jsonPath("$.page.totalElements").value(1))
         .andReturn();
   }
 
