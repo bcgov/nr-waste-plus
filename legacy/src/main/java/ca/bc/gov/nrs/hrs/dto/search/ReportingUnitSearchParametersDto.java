29caba0404af79da40cd2f1f4402e6e37f28f825
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2026-02-10 10:56:43 -0800

    fix: clean up comments in ReportingUnitSearchParametersDto for clarity

--- legacy/src/main/java/ca/bc/gov/nrs/hrs/dto/search/ReportingUnitSearchParametersDto.java
+++ legacy/src/main/java/ca/bc/gov/nrs/hrs/dto/search/ReportingUnitSearchParametersDto.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
-import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.format.annotation.DateTimeFormat;
 
 /**
@@ -16,8 +15,8 @@ import org.springframework.format.annotation.DateTimeFormat;
  *
  * <p>This DTO captures the set of filters the frontend can supply when querying the
  * reporting-unit search endpoint. Several getters are overridden to provide safe, default values
- * when list-based filters are null or empty.</p>
- * Some getters are overridden to provide default values if the lists are null or empty.
+ * when list-based filters are null or empty.</p> Some getters are overridden to provide default
+ * values if the lists are null or empty.
  */
 @Data
 @NoArgsConstructor
