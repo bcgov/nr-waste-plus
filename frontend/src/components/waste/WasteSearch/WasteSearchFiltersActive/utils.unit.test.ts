ac67b3e19c72c60dc9d1860fc5c247154716bfe8
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2026-02-18 09:11:04 -0800

    fix(#508): update display names in mapDisplayFilter for clarity (#509)

--- frontend/src/components/waste/WasteSearch/WasteSearchFiltersActive/utils.unit.test.ts
+++ frontend/src/components/waste/WasteSearch/WasteSearchFiltersActive/utils.unit.test.ts
@@ -7,13 +7,13 @@ describe('mapDisplayFilter', () => {
   it('returns correct display name for known keys', () => {
     expect(mapDisplayFilter('district')).toBe('District');
     expect(mapDisplayFilter('sampling')).toBe('Sampling option');
-    expect(mapDisplayFilter('status')).toBe('Assess area status');
+    expect(mapDisplayFilter('status')).toBe('Status');
     expect(mapDisplayFilter('requestByMe')).toBe('Created By Me');
     expect(mapDisplayFilter('multiMark')).toBe('Multi-mark blocks');
     expect(mapDisplayFilter('requestUserId')).toBe('Submitter');
     expect(mapDisplayFilter('updateDateStart')).toBe('Update Date Start');
     expect(mapDisplayFilter('updateDateEnd')).toBe('Update Date End');
-    expect(mapDisplayFilter('licenseeId')).toBe('Licensee number');
+    expect(mapDisplayFilter('licenseeId')).toBe('Licence number');
     expect(mapDisplayFilter('cuttingPermitId')).toBe('Cutting Permit');
     expect(mapDisplayFilter('timberMark')).toBe('Timber Mark');
     expect(mapDisplayFilter('clientLocationCode')).toBe('Client Location Code');
