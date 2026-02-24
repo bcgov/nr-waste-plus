23d6940d904c31402743b11451c7af581d140945
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2026-02-23 16:46:23 -0800

    fix: remove client name from CodeDescriptionDto mapping

--- legacy/src/main/java/ca/bc/gov/nrs/hrs/mappers/MapperConstants.java
+++ legacy/src/main/java/ca/bc/gov/nrs/hrs/mappers/MapperConstants.java
@@ -40,7 +40,7 @@ public class MapperConstants {
 
   /**
    * MapStruct expression to create a {@code CodeDescriptionDto} from the projection's client
-   * number and client name.
+   * number.
    */
   public static final String CLIENT_AS_DTO =
       "java(new CodeDescriptionDto(projection.getClientNumber(), null))";
