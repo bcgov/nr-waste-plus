80ad21c344f1c878dfeb2933d678daa6c80903b0
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2026-04-01 16:59:51 -0700

    chore: remove TODO comments for property existence check and task recording in shouldHaveStyle function

--- cypress/cypress/support/helpers/uxui.helpers.ts
+++ cypress/cypress/support/helpers/uxui.helpers.ts
@@ -13,10 +13,6 @@ export const shouldHaveStyle = (subject: JQuery<HTMLElement>, property: string, 
       const appStyles = appWindow.getComputedStyle(el);
       const actual = appStyles.getPropertyValue(property).trim();
 
-      //TODO: check if property exist, and fail if not, to avoid false positives when the property is misspelled in the test
-
-      //TODO: add the cy.task recording logic here as well, similar to the one in validateTokenStyle, to log style mismatches that are not related to tokens
-
       const isMissing = !actual;
       const isMismatch = !valuesMatch(expected, actual, rootFontSize);
 
