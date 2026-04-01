b38b58850dbd78da06fa61803e6b178440117336
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2026-03-31 17:00:34 -0700

    chore: remove unused performance tracking code and related steps
    - Deleted `perf.ts` which contained Lighthouse performance tracking interfaces and functions.
    - Removed `uxui.steps.ts` which included button and heading validation steps.
    - Added new accessibility steps in `a11y.steps.ts` for comprehensive a11y checks.
    - Introduced `button.steps.ts` for button interaction steps.
    - Created `common.steps.ts` for common navigation steps.
    - Added `design_token.steps.ts` for design token validation.
    - Introduced `input.steps.ts` for input field interactions.
    - Created `lighthouse.steps.ts` for Lighthouse score validation.
    - Added `text.steps.ts` for text visibility checks.
    - Deleted `tokens.ts` which contained token-related utility functions.

--- cypress/cypress/support/helpers/index.ts
+++ cypress/cypress/support/helpers/index.ts
@@ -2,5 +2,4 @@ export * from "./uxui.helpers";
 export * from "./lighthouse.helpers";
 export * from "./input.helpers";
 export * from "./a11y.helpers";
-export * from "./elements.helper";
-export * from "./design.helpers";
+export * from "./general.helpers";
