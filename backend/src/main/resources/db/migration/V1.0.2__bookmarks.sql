7f354823379e02d7754598f7ca53ac49b9fad4b8
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2026-04-21 11:06:30 -0700

    chore: update user_id column length and add index for reporting_unit_id in user_bookmarks table

--- backend/src/main/resources/db/migration/V1.0.2__bookmarks.sql
+++ backend/src/main/resources/db/migration/V1.0.2__bookmarks.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS hrs.user_bookmarks (
-    user_id    VARCHAR(70) NOT NULL,
+    user_id    VARCHAR(60) NOT NULL,
     reporting_unit_id BIGINT NOT NULL,
     CONSTRAINT user_bookmarks_pk primary key(user_id, reporting_unit_id)
 );
@@ -13,3 +13,6 @@ comment on column hrs.user_bookmarks.user_id is 'Unique identifier for the user'
 comment on column hrs.user_bookmarks.reporting_unit_id is '
 Unique identifier for the reporting unit that the user has bookmarked.
 This is a foreign key referencing the reporting unit in the oracle database.';
+
+-- add an index to speed lookups by reporting_unit_id
+create index if not exists idx_user_bookmarks_reporting_unit_id on hrs.user_bookmarks (reporting_unit_id);
