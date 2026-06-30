ad6a046c7f71c5d3679f0035f5eae32ab78d24b0
Maria Martinez <maria.martinez@gov.bc.ca>
2026-06-30 16:02:56 -0700

    no message

--- cypress/cypress/support/step_definitions/steps/common.steps.ts
+++ cypress/cypress/support/step_definitions/steps/common.steps.ts
@@ -3,7 +3,7 @@ import { Given } from "@badeball/cypress-cucumber-preprocessor";
 Given('I visit {string}', (url: string) => {
   if (url.includes('/search')) {
     cy.intercept('GET', '**/api/codes/districts*').as('getDistricts');
-    cy.intercept('GET', '**/api/codes/samplings*').as('getSampling');
+    cy.intercept('GET', '**/api/codes/samplings*').as('getSamplings');
     cy.intercept('GET', '**/api/codes/assess-area-statuses*').as('getStatuses');
   }
 
@@ -21,6 +21,6 @@ Given('I visit {string}', (url: string) => {
 
   if (url.includes('/search')) {
     // Wait for the reference data to load so dropdowns are populated
-    cy.wait(['@getDistricts', '@getSampling', '@getStatuses'], { timeout: 15000 });
+    cy.wait(['@getDistricts', '@getSamplings', '@getStatuses'], { timeout: 15000 });
   }
 });
