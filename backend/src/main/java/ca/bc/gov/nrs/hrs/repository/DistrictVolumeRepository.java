a5522701615ea18ed2ed2a3ee9db192c68749af0
Maria Martinez <maria.martinez@gov.bc.ca>
2026-07-15 10:36:35 -0700

    feat(be:#1051): Added new methods for species composition

--- backend/src/main/java/ca/bc/gov/nrs/hrs/repository/DistrictVolumeRepository.java
+++ backend/src/main/java/ca/bc/gov/nrs/hrs/repository/DistrictVolumeRepository.java
@@ -6,7 +6,6 @@ import ca.bc.gov.nrs.hrs.entity.districtaveragevolume.DistrictVolumeEntity;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
