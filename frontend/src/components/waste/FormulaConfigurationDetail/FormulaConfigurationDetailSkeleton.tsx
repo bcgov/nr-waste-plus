68eec9f3baa8658cc47aacc0a0be8de6d7d06994
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2026-09-15 15:39:56 -0700

    refactor(#1322): align formula detail layout styles (#1322)

--- frontend/src/components/waste/FormulaConfigurationDetail/FormulaConfigurationDetailSkeleton.tsx
+++ frontend/src/components/waste/FormulaConfigurationDetail/FormulaConfigurationDetailSkeleton.tsx
@@ -3,13 +3,21 @@ import { Column, SkeletonText } from '@carbon/react';
 const FormulaConfigurationDetailSkeleton = () => {
   return (
     <>
-      <Column lg={4} md={4} sm={4} className="district-volume-detail__start-date">
-        <SkeletonText heading width="120px" />
-        <SkeletonText width="80px" />
-      </Column>
-      <Column lg={12} md={4} sm={4} className="district-volume-detail__end-date">
-        <SkeletonText heading width="120px" />
-        <SkeletonText width="80px" />
+      <Column lg={16} md={8} sm={4} className="detail-header">
+        <div className="detail-header__meta">
+          <div>
+            <SkeletonText heading width="120px" />
+            <SkeletonText width="80px" />
+          </div>
+          <div>
+            <SkeletonText heading width="120px" />
+            <SkeletonText width="80px" />
+          </div>
+          <div>
+            <SkeletonText heading width="120px" />
+            <SkeletonText width="80px" />
+          </div>
+        </div>
       </Column>
       <Column lg={16} md={8} sm={4}>
         <SkeletonText heading width="200px" />
